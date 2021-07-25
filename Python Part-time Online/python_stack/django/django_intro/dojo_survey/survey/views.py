e193c24bafe80eba6bbbb3a5fc4b04725f4e5eb0
Op-Design <o.immanuel.o@gmail.com>
2021-07-24 21:48:24 -0500

    Updated dojo_survey

--- Python Part-time Online/python_stack/django/django_intro/dojo_survey/survey/views.py
+++ Python Part-time Online/python_stack/django/django_intro/dojo_survey/survey/views.py
@@ -4,9 +4,14 @@ def survey(request):
     return render(request, "survey.html")
 
 def result(request):
-    if request.method == "POST":
-        test = {
-            "firstName" : request.POST["firstName"]
-            "lastName" : request.POST["lastName"]
+    if request.method == 'POST':
+        context = {
+            'firstName': request.POST['firstName'],
+            'lastName': request.POST['lastName'],
+            'country': request.POST['country'],
+            'dojoLocation': request.POST['dojoLocation'],
+            'comment': request.POST['comment'],
         }
-        return render(request, "result.html", test)
+        print(request.POST)
+        return render(request, 'result.html', context)
+    return render(request, 'result.html')
