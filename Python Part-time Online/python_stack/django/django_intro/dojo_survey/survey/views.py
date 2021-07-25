91dd29710245b53dd6a3ac3a805d00ef3e7b86e5
Op-Design <o.immanuel.o@gmail.com>
2021-07-25 00:09:50 -0500

    Updated dojo_survey

--- Python Part-time Online/python_stack/django/django_intro/dojo_survey/survey/views.py
+++ Python Part-time Online/python_stack/django/django_intro/dojo_survey/survey/views.py
@@ -4,17 +4,27 @@ def survey(request):
     return render(request, "survey.html")
 
 def result(request):
+    request.session['firstName'] = request.POST['firstName'],
+    request.session['lastName'] = request.POST['lastName'],
+    request.session['country'] = request.POST['country'],
+    request.session['dojoLocation'] = request.POST['dojoLocation'],
+    request.session['comment'] = request.POST['comment'],
     return redirect('/session')
 
-def session(request):
+def result2(request):
+    # IMMEDIATELY BELOW is former code used for request.post ...
     if request.method == 'POST':
         context = {
             'firstName': request.POST['firstName'],
             'lastName': request.POST['lastName'],
             'country': request.POST['country'],
             'dojoLocation': request.POST['dojoLocation'],
-            'comment': request.POST['comment'],
+            'comment': request.POST['comment']
         }
         print(request.POST)
         return render(request, 'result.html', context)
     return render(request, 'result.html')
+
+def session(request):
+    return render(request, 'result.html')
+    # return render(request, 'result.html')
